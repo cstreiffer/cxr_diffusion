6145d02a8afa9dcd9b05c43d25453a596a531462
Christopher Streiffer <ccstreiffer@gmail.com>
2024-07-15 00:12:23 -0400

    Adds validation step.

--- util/train.py
+++ util/train.py
@@ -105,7 +105,35 @@ def evaluate(config, step, pipeline):
     os.makedirs(test_dir, exist_ok=True)
     plt.savefig(f"{test_dir}/sample_{step:08d}.png")
 
-def train(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler, loss_fn=F.mse_loss):
+def validate(config, net, noise_scheduler, valid_dataloader, loss_fn):
+    valid_loss = []
+    for step, batch in enumerate(valid_dataloader):
+        clean_images = batch[0]
+
+        # Sample noise to add to the images
+        noise = torch.randn(clean_images.shape, device=clean_images.device)
+        bs = clean_images.shape[0]
+
+        # Sample a random timestep for each image
+        timesteps = torch.randint(
+            0, noise_scheduler.config.num_train_timesteps, (bs,), device=clean_images.device,
+            dtype=torch.int64
+        )
+
+        # Add noise to the clean images according to the noise magnitude at each timestep
+        noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+        with torch.no_grad():
+            # Predict the noise residual
+            if config.has_context:
+                context = batch[1]
+                noise_pred = net(noisy_images, timesteps, class_labels=context, return_dict=False)[0]
+            else:
+                noise_pred = net(noisy_images, timesteps, return_dict=False)[0]
+            loss = loss_fn(noise_pred, noise)
+            valid_loss.append(loss.detach().item())
+    return np.sum(valid_loss) / (step + 1), valid_loss
+
+def train(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler, loss_fn=F.mse_loss, valid_dataloader=None):
     # Initialize accelerator and tensorboard logging
     accelerator = Accelerator(
         mixed_precision=config.mixed_precision,
@@ -130,8 +158,9 @@ def train(config, model, noise_scheduler, optimizer, train_dataloader, lr_schedu
     )
 
     global_step = 0
-    train_loss = []
-    tot_epoch_loss = []
+    train_loss, valid_loss = [], []
+    tot_epoch_loss, tot_val_loss = [], [0]
+
     # Now you train the model
     for epoch in range(config.num_epochs):
         progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
@@ -172,7 +201,7 @@ def train(config, model, noise_scheduler, optimizer, train_dataloader, lr_schedu
             batch_loss = loss.detach().item()
             epoch_loss += batch_loss
             train_loss.append(batch_loss)
-            logs = {"Batch Loss": batch_loss, "Epoch Loss": epoch_loss / (step+1), "Avg Loss": np.mean(train_loss[-1000:]), "LR": lr_scheduler.get_last_lr()[0], "Step": global_step}
+            logs = {"Batch Loss": batch_loss, "Epoch Loss": epoch_loss / (step+1), "Avg Loss": np.mean(train_loss[-1000:]), "LR": lr_scheduler.get_last_lr()[0], "Step": global_step, "Val Loss": tot_val_loss[-1]}
             progress_bar.set_postfix(**logs)
             accelerator.log(logs, step=global_step)
             global_step += 1
@@ -182,7 +211,13 @@ def train(config, model, noise_scheduler, optimizer, train_dataloader, lr_schedu
               pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
               evaluate(config, global_step, pipeline)
 
-        tot_epoch_loss.append(epoch_loss / step)
+        tot_epoch_loss.append(epoch_loss / (step+1))
+
+        # Now compute the validation loss
+        if valid_dataloader is not None:
+            val_epoch_loss, vl  = validate(config, scheduler.unet, noise_scheduler, valid_dataloader, loss_fn)
+            tot_val_loss.append(val_epoch_loss)
+            valid_loss.extend(vl)
 
         # After each epoch you optionally sample some demo images with evaluate() and save the model
         if accelerator.is_main_process:
@@ -202,4 +237,4 @@ def train(config, model, noise_scheduler, optimizer, train_dataloader, lr_schedu
                   'optimizer_state_dict': optimizer.state_dict(),
                   'loss': epoch_loss,
               }, checkpoint_filename)
-    return model, train_loss, tot_epoch_loss
+    return train_loss, valid_loss, tot_epoch_loss, tot_val_loss
